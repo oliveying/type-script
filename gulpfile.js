8fc15df171d127da8e97ba82d72525b676c6b29c
songxiaoying <songxiaoying@tianyancha.com>
2021-08-17 13:57:13 +0800

    add gulp-util & watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
 var gulp = require('gulp');
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 var tsify = require('tsify');
+var gutil = require('gulp-util');
 var paths = {
   pages: ['src/*.html']
 }
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject("tsconfig.json");
-
+// var ts = require('gulp-typescript');
+// var tsProject = ts.createProject("tsconfig.json");
+var watchedBrowserify = watchify(browserify({
+  basedir: '.',
+  debug: true,
+  entries: ['src/main.ts'],
+  cache: {},
+  packageCache: {}
+}).plugin(tsify));
 gulp.task('copy-html', function () {
   return gulp.src(paths.pages)
   .pipe(gulp.dest('dist'))
 })
-gulp.task("default",gulp.series("copy-html"), function () {
-  return browserify({
-    basedir: '.',
-    debug:true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-  })
-  .plugin(tsify)
-  .bundle
+
+function bundle() {
+  return watchedBrowserify
+  .bundle()
   .pipe(source('bundle.js'))
   .pipe(gulp.dest('dist'))
-});
+}
+
+gulp.task("default",gulp.series("copy-html"), bundle);
+watchedBrowserify.on('update', bundle);
+watchedBrowserify.on('log', gutil.log);
